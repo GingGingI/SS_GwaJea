5984c1895a7f58c7a92b875c53dc03d31f07e01a
GingGingI <sma0147@naver.com>
2016-11-11 20:02:22 +0900

    ASDF
    ASDAS

--- MyApplication3/MyApplication/app/src/main/java/com/example/a302/myapplication/MainActivity.java
+++ MyApplication3/MyApplication/app/src/main/java/com/example/a302/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.a302.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -32,4 +33,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         gogo.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        teen= Integer.parseInt(ten.getText().toString());
+
+        switch (v.getId()){
+            case R.id.Btn:
+                aki = (char)teen;
+                aski.setText(aki);
+                Log.i("아스키값" , "가져옴");
+                while(teen < 0){
+                   mid = teen % 2;
+                    sum = mid * i;
+                    teen /= 2;
+                    i*=10;
+                }
+                Log.i("2진법" , "가져옴.");
+                two.setText(String.valueOf(sum));
+                break;
+        }
+    }
 }
